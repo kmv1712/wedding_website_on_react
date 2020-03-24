f4f62650d46316eb974cc0b575c22727ff02c0a0
Mihail <kmv1712@gmail.com>
2020-03-24 21:42:09 +0500

    Переделал навбар

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,87 +1,96 @@
-import React, { Component } from "react";
-import logo from '../../assets/icon/logo192.png';
+import React from "react";
+import logo from "../../assets/icon/logo192.png";
 import { Link, animateScroll as scroll } from "react-scroll";
-import './Navbar.css';
+import "./Navbar.css";
 
 const scrollToTop = () => {
   scroll.scrollToTop();
 };
 
 const Navbar = () => {
-    return (
-      <nav className="nav" id="navbar">
-        <div className="nav-content">
-          <img
-            src={logo}
-            className="nav-logo"
-            alt="Logo"
-            onClick={scrollToTop}
-          />
-          <ul className="nav-items">
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        <img src={logo} width="30" height="30" alt="" />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to="carouselbox"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Главная
+            </Link>
+          </li>
             <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="carouselbox"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Главная
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section2"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Наша история
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section3"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Наши гости
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section4"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                События
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section5"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Section 5
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  };
+                 <Link
+                  className="nav-link"
+                  to="section2"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Наша история
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                className="nav-link"
+                  to="section3"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Наши гости
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                className="nav-link"
+                  to="section4"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  События
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                className="nav-link"
+                  to="section5"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Section 5
+                </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/Components/TextCarouselBox/index.jsx
+++ src/Components/TextCarouselBox/index.jsx
@@ -6,11 +6,9 @@ import './TextCarouselBox.css';
 const TextCarouselBox = () => {
     return (   
         <>                   
-            <div>
-                <div>The Wedding Celebration of</div>
-                <h1>Adam &amp; Hawa</h1>
-                <p>10 September 2018</p>
-            </div>   
+            <div>The Wedding Celebration of</div>
+            <h1>Adam &amp; Hawa</h1>
+            <p>10 September 2018</p>
         </>
     );
 };
